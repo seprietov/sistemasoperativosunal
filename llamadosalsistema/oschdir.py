38c13cf84bf569bd3dd3c02390ecda9d069d1669
Sergio <seprietov@unal.edu.co>
2021-10-31 15:13:46 -0500

    update 2

--- llamadosalsistema/oschdir.py
+++ llamadosalsistema/oschdir.py
@@ -2,6 +2,5 @@ import os
 #os.chdir cambia el directorio de trabajo al indicado en el path que incluye el nombre del nuevo directorio
 path = os.path.join(os.getcwd(),"newDir")
 os.mkdir(path)
-print("aqui se prueba por primera vez")
 print(os.chdir(path))
 print(os.getcwd())
---
+++ llamadosalsistema/osgetcwd.py
@@ -0,0 +1,3 @@
+import os
+# os.getcwd() retorna el directorio en el cual actualmente estamos trabajando
+print(os.getcwd())
