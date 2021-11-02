2f510a227857a20365a996e35c42416ebe1007ae
Sergio <seprietov@unal.edu.co>
2021-11-01 21:08:33 -0500

    update 7

--- procesos/asincronico.py
+++ procesos/asincronico.py
@@ -1,22 +1,22 @@
 import asyncio
 
-async def main():
-    task1 = asyncio.create_task(par())
-    task2 = asyncio.create_task(impar())
-    await par()
-    await impar()
-    print("terminanos")
-
 async def par():
-    for x in range(0, 11, 2):
-        x = x + 2
+    for x in range(2, 11, 2):
         print(x) 
-    await asyncio.sleep(2)
+        x = x + 2
+        await asyncio.sleep(0.2)
+    
 
 async def impar():
-    for x in range(1, 11, 2):
-        x = x + 2
+    for x in range(1, 10, 2):
         print(x)
-    await asyncio.sleep(2)
+        x = x + 2
+        await asyncio.sleep(0.2)
+
+async def main():
+    task1 = asyncio.create_task(impar())
+    task2 = asyncio.create_task(par())
+    await task1
+    print("terminanos")
 
 asyncio.run(main())
